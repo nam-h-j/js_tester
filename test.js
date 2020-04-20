3f96dd757a933131a3f8faf82fb8920a693ccdc6
nam <hzzn74@gmail.com>
2020-04-20 11:39:49 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,31 @@
+$(function(){
+  console.log('1');
+});
+window.onload = function(){
+  var id = 1
+  var index = [1,2,3];
+
+  var idChk = function(id){
+    var find = index.find(function(item){
+      console.log(item == id);
+      return item === id;
+    });
+
+    return find;
+  }
+
+  console.log(idChk(id));
+
+
+  var a = "1"
+  var inventory = ['1','2','3'];
+
+  function findshit(aim){
+    inventory.find(function(item){
+      console.log(item == aim);
+      return item === aim;
+  });
+  }
+  console.log("a :" + a);
+  console.log(findshit(a));
+} //End window.onload
